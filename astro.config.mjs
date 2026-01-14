972adfd44759aecca004d50633d531c0b309e444
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-14 16:16:56 -0500

    añade configuracion basica

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://HC-ONLINE.github.io', 
+  base: '/HC-Architecture',
+  output: 'static',
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});

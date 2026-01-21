0f9d48a6cb2ada895d4bfea44c8d64a98b515776
HC-ONLINE <henriquezandres856@gmail.com>
2026-01-20 21:58:31 -0500

    cambio de dominio

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://HC-ONLINE.github.io', 
-  base: '/HC-Architecture',
+  base: '/',
   output: 'static',
   vite: {
     plugins: [tailwindcss()]
